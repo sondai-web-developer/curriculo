effbac33f4e763526a9ad97f19fc995932123091
Sondai <leonardo.rodrigues199506@gmail.com>
2025-02-03 15:52:33 -0300

    função de destacar item do menu foi adicionada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import nomeProfissoesEmail from "./funcoes/nome_profissoes_email.js";
 import abrirFecharMenu from "./funcoes/abrir_fechar_menu.js";
+import destacarItemMenu from "./funcoes/destacar_item_menu.js";
 
 nomeProfissoesEmail();
 abrirFecharMenu();
+destacarItemMenu();
